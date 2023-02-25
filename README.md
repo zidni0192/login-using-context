2535066fd85f7314863d3da3b9d4c171834a1166
Your Name <you@example.com>
2023-02-25 21:38:35 +0700

    update : resize login input field, title, reformat code

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Login page
-![img_1.png](img_1.png)
-## Welcome page
 ![img.png](img.png)
+## Welcome page
+![img_1.png](img_1.png)
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import React, {useCallback, useContext} from 'react';
 import {UserContext} from "../context/user.context";
+import loginStyle from './login.module.css'
 
 const Login = () => {
     const {setUser} = useContext(UserContext)
@@ -14,49 +15,24 @@ const Login = () => {
     }, [])
     return (
         <form onSubmit={handleSubmit}>
-            <div style={{
-                display: 'flex',
-                alignItems: 'center',
-                flexDirection: 'column',
-                height: '100vh',
-                justifyContent: 'center',
-                gap: '20px'
-            }}>
-                <h1 className={'header-title'}>OBS REACTJS TEST</h1>
+            <div className={loginStyle.container}>
+                <h1 className={loginStyle.headerTitle}>OBS REACTJS TEST</h1>
                 <input
                     type={'text'}
                     name={'user_id'}
                     placeholder={"User ID"}
-                    style={{
-                        textAlign: 'center',
-                        padding: '10px 20px',
-                        borderRadius: '8px',
-                        border: '1px solid #4472C4'
-                    }}
+                    className={loginStyle.inputText}
                     required
                 />
                 <input
                     type={'password'}
                     name={'password'}
                     placeholder={"Password"}
-                    style={{
-                        textAlign: 'center',
-                        padding: '10px 20px',
-                        borderRadius: '8px',
-                        border: '1px solid #4472C4'
-                    }}
+                    className={loginStyle.inputText}
                     required
                 />
-                <button type={"submit"} style={{
-                    padding: '10px 20px',
-                    background: 'linear-gradient(#9DC3E6,#609ED6,#2A69A2)',
-                    color: "#FFF",
-                    borderRadius: "8px",
-                    border: "0px",
-                    fontWeight: '700',
-                    cursor: "pointer",
-                    marginTop: "25px"
-                }}>Sign In
+                <button type={"submit"} className={loginStyle.buttonSubmit}>
+                    Sign In
                 </button>
                 <a href={"#"}>Forgot My Password</a>
             </div>
